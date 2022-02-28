5f51b8aab0f3d24f8b405692f0f6914757c545fd
Mario0302 <m.manolov2017@pmg-pernik.com>
2022-02-28 16:03:38 +0200

    Second commit

---
+++ HotelsBG/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HotelsBG.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HotelsBG.Areas.Identity.IdentityHostingStartup))]
+namespace HotelsBG.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
